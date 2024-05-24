3d241a2fb3271ac6b92340bf32b0c960f7f39e0f
S48avio <151058808+S48avio@users.noreply.github.com>
2024-05-24 14:47:29 +0530

    Update medicine.js

--- src/medicine.js
+++ src/medicine.js
@@ -1,1 +1,31 @@
 
+const mongoose = require('mongoose');
+const connect = mongoose.connect('mongodb+srv://saviosunny48:2TJsNwpNwqJX2aG3@cluster0.0zmwv1l.mongodb.net/');
+connect.then(()=>{
+    console.log("Database connection established hbducyydbbyub");
+})
+.catch(()=>{
+    console.log("Database connection is not established");
+})
+
+// Define the schema for medicines
+const MedicineSchema = new mongoose.Schema({
+    medicines: [
+        {
+            medicine: {
+                type: String,
+                required: true
+            },
+            cost: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+});
+
+// Create a model for the Medicine collection
+const Medicine = mongoose.model('Medicine', MedicineSchema);
+console.log(Medicine)
+
+module.exports = Medicine;
